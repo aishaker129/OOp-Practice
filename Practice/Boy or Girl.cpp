de9d0e31b326d1ec6d94b9b8513ac6c24a09b8d3
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2022-08-18 15:46:48 +0600

    Create Boy or Girl.cpp

---
+++ Practice/Boy or Girl.cpp
@@ -0,0 +1,32 @@
+//problem no.236A
+//problem name:Boy or Girl
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    int b=0;
+    cin>>s;
+     sort(s.begin(), s.end());
+    for(int i=0;i<s.size();i++)
+    {
+
+
+           if(s[i]!=s[i+1])
+
+       {
+           b++;
+       }
+    }
+    if(b%2==0)
+    {
+        cout<<"CHAT WITH HER!"<<endl;
+    }
+    else
+    {
+        cout<< "IGNORE HIM!"<<endl;
+    }
+    return 0;
+}
