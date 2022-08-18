37eb485f22449a28361bfb1901a6597997387ff3
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2022-08-18 15:46:05 +0600

    Create Bit++.cpp

---
+++ Practice/Bit++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n, x = 0;
+	string ch;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> ch;
+		if (ch[1]=='+')
+			x++;
+		else
+			x--;
+	}
+	cout << x << "\n";
+
+	return 0;
+}
