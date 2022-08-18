836daa78ae13910a62d5aacc4705ef97f64667b5
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2022-08-18 15:55:09 +0600

    Create Chess Time.cpp

---
+++ Practice/Chess Time.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int n,c=20;
+        cin>>n;
+        cout<<(n*60)/20<<endl;
+
+    }
+    return 0;
+}
