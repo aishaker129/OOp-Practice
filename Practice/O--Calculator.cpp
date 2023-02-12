338c63ee1abab8a7352f7ca004705f21533bdc5e
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2023-02-13 01:41:30 +0600

    Create O--Calculator.cpp

---
+++ Practice/O--Calculator.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+
+int main(){
+
+ int a,b;char c;
+ cin>>a>>c>>b;
+ if(c=='+') cout<<a+b<<endl;
+
+  if(c=='-') cout<<a-b<<endl;
+  if(c=='*') cout<<a*b<<endl;
+  if(c=='/') cout<<a/b<<endl;
+
+
+return 0;
+}
